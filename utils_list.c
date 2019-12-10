311201ee24c5e6e69479eda7e9fb22bb35490c2f
Marius <marius.contoli-legay@epitech.eu>
2019-12-10 19:21:16 +0100

    add utilitary list file

---
+++ utils_list.c
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2019
+** utils_list.c
+** File description:
+** utilitary file for lists
+*/
+
+#include "include/my_push_swap.h"
+
+list_t new_list(void)
+{
+    return NULL;
+}
+
+Bool is_empty_list(list_t l_x)
+{
+    if (l_x == NULL) {
+        return true;
+    }
+    return false;
+}
+
+int my_listlen(list_t l_x)
+{
+    if (is_empty_list(l_x)) {
+        return (0);
+    }
+    return l_x->length;
+}
+
+int my_first_elem(list_t l_x)
+{
+    if (is_empty_list(l_x)) {
+        exit (1);
+    }
+    return l_x->start->number;
+}
+
+int my_last_elem(list_t l_x)
+{
+    if (is_empty_list(l_x)) {
+        exit (1);
+    }
+    return l_x->end->number;
+}
